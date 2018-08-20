d4038fc803b344c1127e73bb8667060e85bd8b1d
Vitaly Karpenko <v.karpenko@atol.ru>
2018-08-20 13:47:42 +0300

    Add gauges

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='async-statsd',
-    version='0.1.4',
+    version='0.1.5',
     description='Модуль для асинхронной отправки метрик в Statsd по UDP',
     packages=['async_statsd'],
     license='MIT License',
